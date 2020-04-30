521dc6e71e06fe5c3a5c9aaccd5b29edeae1395e
Rahul <rahul.sharma@pharmeasy.in>
2020-04-30 13:31:39 +0530

    add

--- app.js
+++ app.js
@@ -1,68 +1,83 @@
+window.addEventListener('load', function(){
+    var btnRoll = document.querySelector(".btn-roll");
+    var btnHold = document.querySelector(".btn-hold");
+    var btnNew = document.querySelector(".btn-new");
+    var dice = document.querySelector("#dice");
+    var player0 = document.querySelector("#player-0");
+    var player1 = document.querySelector("#player-1");
+    var activePlayer = 0;
+    var roundScore = 0;
+    var score = [0,0];
 
-var btnRoll = document.querySelector(".btn-roll");
-var btnHold = document.querySelector(".btn-hold");
-var btnNew = document.querySelector(".btn-new");
-var dice = document.querySelector("#dice");
-var player0 = document.querySelector("#player-0");
-var player1 = document.querySelector("#player-1");
-var activePlayer = 0;
-var roundScore = 0;
-var score = [0,0];
+
+    btnNew.addEventListener("click", function() {
+        btnRoll.classList.remove("hide");
+        btnHold.classList.remove("hide");
+        dice.classList.remove("hide");
+        document.querySelector("#player-"+activePlayer).classList.add("active");
+    });
+
+    btnRoll.addEventListener("click", function () {
+        var random = Math.ceil(Math.random()*6);
+        dice.src = "dice-"+random+".png";
+        dice.classList.remove("hide");
+        if(random == 1) {
+            changePlayer();
+        } else  {
+            var currentRound = document.querySelector("#round-"+activePlayer)
+            roundScore += random;
+            currentRound.textContent = roundScore
+        }
+    });
 
 
-btnNew.addEventListener("click", function() {
-    btnRoll.classList.remove("hide");
-    btnHold.classList.remove("hide");
-    dice.classList.remove("hide");
-    document.querySelector("#player-"+activePlayer).classList.add("active");
-});
+    btnHold.addEventListener("click", function () {
+        score[activePlayer] += roundScore;
+        if(score[activePlayer] >= 100) {
+            alert("Player "+(activePlayer+1)+" won !");
+            resetGame();
+        } else {
+            document.querySelector("#score-"+activePlayer).textContent = score[activePlayer];
+            dice.classList.add("hide");
+            changePlayer();
+        }
+    });
 
-btnRoll.addEventListener("click", function () {
-    var random = Math.ceil(Math.random()*6);
-    dice.src = "dice-"+random+".png";
-    dice.classList.remove("hide");
-    if(random == 1) {
-        changePlayer();
-    } else  {
-        var currentRound = document.querySelector("#round-"+activePlayer)
-        roundScore += random;
-        currentRound.textContent = roundScore
+
+    function changePlayer() {
+        document.querySelector("#round-"+activePlayer).textContent = 0;
+        activePlayer = activePlayer === 0 ? 1:0;
+        player0.classList.toggle("active");
+        player1.classList.toggle("active");
+        roundScore = 0;
     }
-});
 
-
-btnHold.addEventListener("click", function () {
-    score[activePlayer] += roundScore;
-    if(score[activePlayer] >= 100) {
-        alert("Player "+(activePlayer+1)+" won !");
-        resetGame();
-    } else {
-        document.querySelector("#score-"+activePlayer).textContent = score[activePlayer];
+    function resetGame() {
+        score = [0,0];
+        activePlayer = 0;
+        roundScore = 0;
+        btnRoll.classList.add("hide");
+        btnHold.classList.add("hide");
         dice.classList.add("hide");
-        changePlayer();
+        player0.classList.remove("active");
+        player1.classList.remove("active");
+        document.querySelector("#round-0").textContent = '0';
+        document.querySelector("#round-1").textContent = '0';
+        document.querySelector("#score-0").textContent = '0';
+        document.querySelector("#score-1").textContent = '0';
     }
 });
 
-
-function changePlayer() {
-    document.querySelector("#round-"+activePlayer).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1:0;
-    player0.classList.toggle("active");
-    player1.classList.toggle("active");
-    roundScore = 0;
+function preloadImage (url) {
+    try {
+        var _img = new Image();
+        _img.src = url;
+    } catch (e) { }
 }
 
-function resetGame() {
-    score = [0,0];
-    activePlayer = 0;
-    roundScore = 0;
-    btnRoll.classList.add("hide");
-    btnHold.classList.add("hide");
-    dice.classList.add("hide");
-    player0.classList.remove("active");
-    player1.classList.remove("active");
-    document.querySelector("#round-0").textContent = '0';
-    document.querySelector("#round-1").textContent = '0';
-    document.querySelector("#score-0").textContent = '0';
-    document.querySelector("#score-1").textContent = '0';
-}
+preloadImage("dice-1.png");
+preloadImage("dice-2.png");
+preloadImage("dice-3.png");
+preloadImage("dice-4.png");
+preloadImage("dice-5.png");
+preloadImage("dice-6.png");
